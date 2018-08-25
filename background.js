40d4c6d37be63b63befb0f2d00a1dc5159af5b9b
laveesingh <lavees96@gmail.com>
2018-08-25 20:02:49 +0530

    Add :: added keyboard shortcuts with modifier keys

--- background.js
+++ background.js
@@ -12,7 +12,28 @@ chrome.runtime.onInstalled.addListener(function() {
 })
 
 chrome.commands.onCommand.addListener(function(command) {
-  sendMessageToAllTabs({ message: command })
+  let message = ''
+  switch (command) {
+    case 'playNext':
+      message = 'playNext'
+      break
+    case 'playNextDup':
+      message = 'playNext'
+      break
+    case 'playPrev':
+      message = 'playPrev'
+      break
+    case 'playPrevDup':
+      message = 'playPrev'
+      break
+    case 'togglePause':
+      message = 'togglePause'
+      break
+    case 'togglePauseDup':
+      message = 'togglePause'
+      break
+  }
+  sendMessageToAllTabs({ message })
 })
 
 function sendMessageToAllTabs({ message }) {
